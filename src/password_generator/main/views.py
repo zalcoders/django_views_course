08632fd106aaa6cf337818e990c85c8b0c67a06f
= <=>
2025-02-05 17:38:43 +0300

    password generator 2

--- src/password_generator/main/views.py
+++ src/password_generator/main/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
 
 
+def calculate_password(length, uppercase, numbers, symbols):
+    print(length, uppercase, numbers, symbols)
+
+
 def password_generator_view(request):
+    if request.method == "POST":
+        length = request.POST.get("length")
+        uppercase = True if request.POST.get("uppercase") else False
+        numbers = True if request.POST.get("numbers") else False
+        symbols = True if request.POST.get("symbols") else False
+
+        calculate_password(length, uppercase, numbers, symbols)
+    else:
+        pass
     return render(request, "main/index.html", {})
