baff8e6f96df27d6a564950797b5be60696c441a
= <=>
2025-02-07 15:52:35 +0300

    shortner 4

--- src/zal_shortner/shortner/views.py
+++ src/zal_shortner/shortner/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
 
+from shortner.models import ShortenedURL
 
 def home_page(request):
-    return render(request, "shortner/index.html", {})
+    if request.method == "GET":
+        return render(request, "shortner/index.html", {})
 
+    url = request.POST.get("url")
+    if not url:
+        return render(request, "shortner/index.html", {"error": "Invalid URL"})
+    
+    s = ShortenedURL(main_url=url)
+    s.save()
+
+    return render(request, "shortner/result.html", {"url": s})
+   
 def result(request):
     return render(request, "shortner/result.html", {})
