abdfe29b9d302aec469bd5fa6b6c4346e1a98180
= <=>
2025-02-07 18:37:23 +0300

    shortner 7

--- src/zal_shortner/shortner/urls.py
+++ src/zal_shortner/shortner/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from shortner.views import home_page, result, redirect_view
+from shortner.views import home_page, result, redirect_view, dashboard
 
 
 app_name = "shortner"
@@ -8,4 +8,5 @@ urlpatterns = [
     path('result/', result),
     path('', home_page, name="generate-new-url"),
     path('<short_url_id:pk>/', redirect_view, name="short-url"),
+    path('dashboard/<short_url_id:pk>/', dashboard, name="short-url-dashboard"),
 ]
--- src/zal_shortner/shortner/views.py
+++ src/zal_shortner/shortner/views.py
@@ -1,15 +1,16 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from django.http import HttpResponse
 
 from shortner.models import ShortenedURL
 
 def home_page(request):
+    recent_links = ShortenedURL.objects.order_by("-created_at")[:2]
+
     if request.method == "GET":
-        return render(request, "shortner/index.html", {})
+        return render(request, "shortner/index.html", {"recent_links": recent_links})
 
     url = request.POST.get("url")
     if not url:
-        return render(request, "shortner/index.html", {"error": "Invalid URL"})
+        return render(request, "shortner/index.html", {"error": "Invalid URL", "recent_links": recent_links})
     
     s = ShortenedURL(main_url=url)
     s.save()
@@ -22,4 +23,12 @@ def result(request):
 
 def redirect_view(request, pk):
     s = get_object_or_404(ShortenedURL, pk=pk)
+    s.total_clicks += 1
+    s.save()
     return redirect(s.main_url)
+
+
+def dashboard(request, pk):
+    s = get_object_or_404(ShortenedURL, pk=pk)
+    return render(request, "shortner/result.html", {"url": s})
+
