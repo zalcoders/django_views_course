f59dea613121384583ed109e09950535a7d35dc4
= <=>
2025-02-13 10:52:34 +0300

    current time 5

--- src/zal_time/current_time/views.py
+++ src/zal_time/current_time/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponseNotFound, HttpResponse, Http404
+from django.http import Http404
 import datetime as dt
 from django.views.decorators.http import require_http_methods
+
+import pytz
 
 ALLOWED_TIMEZONES = [
     "UTC",
@@ -19,12 +21,22 @@ ALLOWED_TIMEZONES = [
 
 # @require_http_methods(["POST"])
 def home(request):
-    print(request.POST)
     if request.method == "POST":
         selected_timezone = request.POST.get("timezone", "")
 
         if not selected_timezone or selected_timezone not in ALLOWED_TIMEZONES:
             raise Http404("Time Zone Not Found!")
+        
+        request.session["timezone"] = selected_timezone
 
-    context = {"current_dt": dt.datetime.now().strftime("%Y-%m-%dT%H:%M:%S")}
-    return render(request, "current_time/home.html", context)
+    selected_timezone = request.session.get("timezone", "UTC")
+
+    timezone = pytz.timezone(selected_timezone)
+    current_time = dt.datetime.now(timezone)
+
+    context = {
+        "current_dt": current_time.strftime("%Y-%m-%dT%H:%M:%S"),
+        "selected_timezone": selected_timezone,
+        }
+    response = render(request, "current_time/home.html", context)
+    return response
