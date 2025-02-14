f6aa1b40d1d5136e0261bad93d208b110b8e56a7
= <=>
2025-02-14 18:50:50 +0300

    Zal Upload 3

--- src/zal_upload/uploader/urls.py
+++ src/zal_upload/uploader/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from uploader.views import upload, success, download
+from uploader.views import upload, success, download, test_view
 
 app_name = "shortner"
 urlpatterns = [
     path('', upload, name="upload"),
     path('success/<uuid:file_uuid>/', success, name="success"),
     path('download/<uuid:file_uuid>', download, name="download"),
+
+    path('test', test_view, name="test"),
 ]
--- src/zal_upload/uploader/views.py
+++ src/zal_upload/uploader/views.py
@@ -2,6 +2,13 @@ from django.shortcuts import render
 
 
 def upload(request):
+    f = request.FILES["my_file"]
+
+    file_path = f"/home/zalcoders/projects/Recording/DjangoViews/tmp/{f.name}"
+    with open(file_path, "wb") as destination_file:
+        for chunk in f.chunks():
+            destination_file.write(chunk)
+
     return render(request, "uploader/upload.html", {})
 
 def success(request, file_uuid):
@@ -9,3 +16,6 @@ def success(request, file_uuid):
 
 def download(request, file_uuid):
     return render(request, "uploader/download.html", {})
+
+def test_view(request):
+    return render(request, "uploader/test.html", {})
