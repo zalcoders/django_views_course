ef6c25da4de096f6758c26f8ec06e818fe644df0
= <=>
2025-02-25 17:48:23 +0300

    Zal Upload 15

--- src/zal_upload/scripts/remove_old_files_s3.py
+++ src/zal_upload/scripts/remove_old_files_s3.py
@@ -1,11 +1,54 @@
+import boto3
+from botocore.exceptions import ClientError
+import datetime as dt
+
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "zal_upload.settings")
+import pytz
 
-import django
-django.setup()
 
-from uploader.models import UploaddedFile
+S3_URL = os.environ.get("S3_URL", "https://storage.c2.liara.space")
 
-all = UploaddedFile.objects.all()
+S3_ACCESS_KEY = os.environ.get("S3_ACCESS_KEY", "1fdgs0o86pbdgtvv")
+S3_SECRET_KEY = os.environ.get("S3_SECRET_KEY", "2699e820-9cad-42ac-96a9-4485ea20da17")
 
-print(all)
+
+SECONDS_IN_DAYS = 24 * 60 * 60
+
+
+def list_expired_files_s3(bucket, life_time=SECONDS_IN_DAYS*2):
+    s3_client = boto3.client(
+        "s3",
+        endpoint_url=S3_URL,
+        aws_access_key_id=S3_ACCESS_KEY,
+        aws_secret_access_key=S3_SECRET_KEY,
+    )
+
+    expiration_date = dt.datetime.now() - dt.timedelta(seconds=life_time)
+
+    paginator = s3_client.get_paginator("list_objects_v2")
+
+    expired_files = []
+    for page in paginator.paginate(Bucket=bucket, PaginationConfig={'PageSize': 50}):
+        if "Contents" in page:
+            for obj in page["Contents"]:
+                if obj["LastModified"] < expiration_date.replace(tzinfo=pytz.UTC):
+                    expired_files.append(obj["Key"])
+
+    return expired_files
+
+
+def remove_expired_files_s3(bucket, keys):
+    s3_client = boto3.client(
+        "s3",
+        endpoint_url=S3_URL,
+        aws_access_key_id=S3_ACCESS_KEY,
+        aws_secret_access_key=S3_SECRET_KEY,
+    )
+
+    for key in keys:
+        response = s3_client.delete_object(Bucket=bucket, Key=key)
+
+
+expired_files = list_expired_files_s3("zalcoders-upload")
+print(len(expired_files))
+remove_expired_files_s3("zalcoders-upload", expired_files)
